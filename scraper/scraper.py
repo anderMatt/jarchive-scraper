9cb01aa1c0790ece6041453fad461eae572e328f
matthewanderson <matt10.anderson@gmail.com>
2017-01-03 15:37:21 -0500

    Successfully parse value, question, and answer from each clue node.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 import bs4
-import requests
 import queue
+import re
+import requests
 import threading
+from collections import namedtuple
+
+CLUE_ANSWER_REGEX = re.compile(r'''<em class="correct_response">(.+)</em>''')
+
+Clue = namedtuple('Clue', ['value', 'question', 'answer'])
+
+class MalformedRoundHTMLError(Exception):
+    """Raise when the HTML of a JArchive round is not properly formatted.
+
+    The HTML of each jeopardy round should contain 6 category nodes and 30 clue nodes. The HTML of some gamerounds
+    contains mismatched tags, usually involving nested <a> and <i> tags in the question text of a clue. Beautiful Soup
+    cannot properly parse this invalid HTML, so the scraper is unable to serialize the round.
+    """
+    pass
+
+class IncompleteClueError(Exception):
+    """Raise when a (value AND question AND answer) cannot be parsed from a clue node.
+
+    Many game clues on JArchive, especially for very recent games, are incomplete.
+    """
+    pass
+
 
 def get_page_soup(url):
     """Returns bs4.BeautifulSoup object of page at url."""
+
     try:
         req = requests.get(url)
         req.raise_for_status()
@@ -15,13 +39,88 @@ def get_page_soup(url):
     page_soup = bs4.BeautifulSoup(req.text, "html.parser")
     return page_soup
 
+
 def get_jeopardy_rounds(page_soup):
     return page_soup.find_all("table", class_="round")
 
-def get_category_titles(round_soup):
-    category_title_nodes = round_soup.find_all("td", class_="category_name")
-    category_titles = [c.text for c in category_title_nodes or None] # If list does not contain six elements, clues will be associated with the wrong category in <FUNCNAME>. 'Padding' the list with None when a title string does not exist ensures len=6. 
 
+def get_round_categories(round_soup):
+    """Returns a list of the round's categories.
+
+    The list must contain six elements, or else clues will be associated with the wrong category in <FUNCNAME>. 'Padding' the list with None
+    when a title string is missing for a category ensures len=6.
+    """
+
+    category_title_nodes = round_soup.find_all("td", class_="category_name")
+    category_titles = [c.text for c in category_title_nodes or None]
+    return category_titles
+
+def get_round_clue_nodes(round_soup):
+    return round_soup.find_all("td", class_="clue")
+
+def parse_clue_value(clue_node):
+    value_node = clue_node.find("td", class_="clue_value") or clue_node.find("td", class_="clue_value_daily_double")
+    if value_node is None:
+        return None
+    value = value_node.text.strip('$')
+    if value.startswith('D'):
+        value = "daily_double"
+    return value
+
+
+def parse_clue_question(clue_node):
+    question_node = clue_node.find("td", class_="clue_text")
+    if question_node is None:
+        return None
+    question = question_node.text
+    return question
+
+
+def parse_clue_answer(clue_node):
+    answer_node = clue_node.find('div')
+    try:
+        answer = CLUE_ANSWER_REGEX.search(answer_node['onmouseover']).group(1)
+    except AttributeError:  # Could not parse answer
+        return None
+    return answer
+
+
+def serialize_clue_node(clue_node):
+    value = parse_clue_value(clue_node)
+    if not value:
+        raise IncompleteClueError
+    question = parse_clue_question(clue_node)
+    if not question:
+        raise IncompleteClueError
+    answer = parse_clue_answer(clue_node)
+    if not answer:
+        raise IncompleteClueError
+    return {"value": value, "question": question, "answer": answer}
+
+
+def serialize_jeopardy_round(round_soup):
+    """
+    Returns:
+        A list of dictionaries that each represent a category of the given round. Each dictionary has the interface
+        {'title': category_title_string, 'clues': list_of_serialized_clues}
+    
+    Raises:
+        MalformedRoundHTMLError: If Beautiful Soup cannot parse 6 category nodes and/or 30 clue nodes from round_soup.
+    """
+
+    round_dict = {}
+    category_titles = get_round_categories(round_soup)
+    round_clue_nodes = get_round_clue_nodes(round_soup) 
+    if not (len(category_titles) == 6 and len(round_clue_nodes) == 30):
+        raise MalformedBoardHTMLError
+    for (category_index, category_title) in enumerate(category_titles):
+        category_clue_nodes = [round_clue_nodes[i] for i in range(0,30,category_index)]
+        try:
+            serialized_category_clues = map(lambda c: serialize_clue_node(c), category_clue_nodes)
+        except IncompleteClueError:
+            return
+
+    return
 
 
 if __name__ == "__main__":
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -2,12 +2,18 @@
 
 #generic imports
 import os
-import unittest
 import bs4
+import unittest
+import mock
 
 #test imports
 from scraper.scraper import (get_jeopardy_rounds,
-        get_category_titles)
+        get_round_categories,
+        parse_clue_value,
+        parse_clue_question,
+        parse_clue_answer,
+        serialize_clue_node,
+        IncompleteClueError)
 
 TEST_HTML_PAGE = "test_page.html" # File containing markup of a JArchive page to test scraper against.
 
@@ -16,21 +22,73 @@ test_html_page_path = "{}/{}".format(current_dir, TEST_HTML_PAGE)
 
 @unittest.skipIf(not os.path.isfile(test_html_page_path), 'Test html page not in directory.')
 class TestScraper(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         with open(test_html_page_path, 'r') as markup:
             cls.page_soup = bs4.BeautifulSoup(markup, "html.parser")
+        clue_nodes = cls.page_soup.find_all("td", class_="clue")
+        cls.clue_node = clue_nodes[3]
+        cls.daily_double_clue_node = clue_nodes[19]
+
 
     def test_get_jeopardy_rounds(self):
         rounds = get_jeopardy_rounds(self.page_soup)
         expected_number_rounds = 2
         self.assertEqual(len(rounds), expected_number_rounds)
 
-    def test_get_category_titles(self):
+    def test_get_round_categories(self):
         game_round = get_jeopardy_rounds(self.page_soup)[0]
-        titles = get_category_titles(game_round)
-        number_of_categories = 6
-        self.assertEqual(len(titles), number_of_categories)
+        titles = get_round_categories(game_round)
+        self.assertEqual(len(titles), 6)
+        expected_titles = [
+                "MUCH BIGGER THAN A BREADBOX",
+                "LITERARY LINES",
+                "THE BROADWAY MUSICAL'S CHARACTERS",
+                "REPRESENTIN'",
+                "TREES",
+                'A TIME FOR "US"'
+                ]
+        for title, expected_title in zip(titles, expected_titles):
+            self.assertEqual(title, expected_title)
+
+    def test_parse_clue_value(self):
+        expected_value = "200"
+        value = parse_clue_value(self.clue_node)
+        self.assertEqual(value, expected_value)
+
+    def test_parse_clue_value_daily_double(self):
+        value = parse_clue_value(self.daily_double_clue_node)
+        expected_value = "daily_double"
+        self.assertEqual(value, expected_value)
+
+    def test_parse_clue_question(self):
+        question = parse_clue_question(self.clue_node)
+        expected_question = 'This Speaker of the House helped draft the "Contract With America"'
+        self.assertEqual(question, expected_question)
+
+    def test_parse_clue_answer(self):
+        answer = parse_clue_answer(self.clue_node)
+        expected_answer = "Newt Gingrich"
+        self.assertEqual(answer, expected_answer)
+    
+    @mock.patch('scraper.scraper.parse_clue_value')
+    def test_serialize_clue_node_raises_error_when_value_missing(self, mock_value):
+        mock_value.return_value = None
+        with self.assertRaises(IncompleteClueError):
+            serialize_clue_node(self.clue_node)
+    
+    @mock.patch('scraper.scraper.parse_clue_question')
+    def test_raises_when_clue_question_missing(self, mock_value):
+        mock_value.return_value = None
+        with self.assertRaises(IncompleteClueError):
+            serialize_clue_node(self.clue_node)
+    
+    @mock.patch('scraper.scraper.parse_clue_value')
+    def test_raises_when_clue_answer_missing(self, mock_value):
+        mock_value.return_value = None
+        with self.assertRaises(IncompleteClueError):
+            serialize_clue_node(self.clue_node)
 
 
 
