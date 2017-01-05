68b35842b40694727b209f6c2fa282d399eed148
matthewanderson <matt10.anderson@gmail.com>
2017-01-05 18:23:56 -0500

    Update unit tests to work with new directory structure.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,13 +7,13 @@ import unittest
 import mock
 
 #test imports
-from scraper.scraper import (get_jeopardy_rounds,
-        get_round_categories,
-        parse_clue_value,
-        parse_clue_question,
-        parse_clue_answer,
-        serialize_clue_node,
-        IncompleteClueError)
+from scraper.parser import (_get_jeopardy_rounds,
+        _get_round_categories,
+        _parse_clue_question,
+        _parse_clue_answer,
+        _serialize_clue_node
+        )
+from scraper.exceptions import IncompleteClueError
 
 TEST_HTML_PAGE = "test_page.html" # File containing markup of a JArchive page to test scraper against.
 
@@ -29,17 +29,15 @@ class TestScraper(unittest.TestCase):
             cls.page_soup = bs4.BeautifulSoup(markup, "html.parser")
         clue_nodes = cls.page_soup.find_all("td", class_="clue")
         cls.clue_node = clue_nodes[3]
-        cls.daily_double_clue_node = clue_nodes[19]
-
 
     def test_get_jeopardy_rounds(self):
-        rounds = get_jeopardy_rounds(self.page_soup)
+        rounds = _get_jeopardy_rounds(self.page_soup)
         expected_number_rounds = 2
         self.assertEqual(len(rounds), expected_number_rounds)
 
     def test_get_round_categories(self):
-        game_round = get_jeopardy_rounds(self.page_soup)[0]
-        titles = get_round_categories(game_round)
+        game_round = _get_jeopardy_rounds(self.page_soup)[0]
+        titles = _get_round_categories(game_round)
         self.assertEqual(len(titles), 6)
         expected_titles = [
                 "MUCH BIGGER THAN A BREADBOX",
@@ -52,43 +50,27 @@ class TestScraper(unittest.TestCase):
         for title, expected_title in zip(titles, expected_titles):
             self.assertEqual(title, expected_title)
 
-    def test_parse_clue_value(self):
-        expected_value = "200"
-        value = parse_clue_value(self.clue_node)
-        self.assertEqual(value, expected_value)
-
-    def test_parse_clue_value_daily_double(self):
-        value = parse_clue_value(self.daily_double_clue_node)
-        expected_value = "daily_double"
-        self.assertEqual(value, expected_value)
-
     def test_parse_clue_question(self):
-        question = parse_clue_question(self.clue_node)
+        question = _parse_clue_question(self.clue_node)
         expected_question = 'This Speaker of the House helped draft the "Contract With America"'
         self.assertEqual(question, expected_question)
 
     def test_parse_clue_answer(self):
-        answer = parse_clue_answer(self.clue_node)
+        answer = _parse_clue_answer(self.clue_node)
         expected_answer = "Newt Gingrich"
         self.assertEqual(answer, expected_answer)
     
-    @mock.patch('scraper.scraper.parse_clue_value')
-    def test_serialize_clue_node_raises_error_when_value_missing(self, mock_value):
-        mock_value.return_value = None
-        with self.assertRaises(IncompleteClueError):
-            serialize_clue_node(self.clue_node)
-    
-    @mock.patch('scraper.scraper.parse_clue_question')
+    @mock.patch('scraper.parser._parse_clue_question')
     def test_raises_when_clue_question_missing(self, mock_value):
         mock_value.return_value = None
         with self.assertRaises(IncompleteClueError):
-            serialize_clue_node(self.clue_node)
+            _serialize_clue_node(self.clue_node)
     
-    @mock.patch('scraper.scraper.parse_clue_value')
+    @mock.patch('scraper.parser._parse_clue_answer')
     def test_raises_when_clue_answer_missing(self, mock_value):
         mock_value.return_value = None
         with self.assertRaises(IncompleteClueError):
-            serialize_clue_node(self.clue_node)
+            _serialize_clue_node(self.clue_node)
 
 
 
